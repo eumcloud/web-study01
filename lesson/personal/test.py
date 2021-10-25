53e0467ba480825b9e899fd596b0cf4ab3f038db
euncloud <livelch012@naver.com>
2021-10-25 20:07:07 +0900

    Ready for py

---
+++ lesson/personal/test.py
@@ -0,0 +1,10 @@
+print('hello')
+print('hardware')
+print('tele')
+print('software')
+print('Abstraction')
+
+
+print('\n 1. Variable')
+print('\n 2. Function')
+print('\n example we are used function that print()')
