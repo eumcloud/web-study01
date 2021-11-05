229b7e8addc8e30ccecc73f8d6abafeef80a5258
- <livelch012@naver.com>
2021-11-05 12:50:41 +0000

    URL요청값에 따라 다른 라우팅 예제

---
+++ lesson/kiprojecti/assets/js/route.js
@@ -0,0 +1,18 @@
+// URL에 따라서 다른응답을 주도록 세팅
+
+// 서버입장에서, URL정보(클라이언트가 요청)는 request객체의 속성을 통해 들어온다. **받는 정보중 path부터 중요한 정보이다.
+
+const http = require("http");
+users = ['Tom', 'Andy', 'Jessica', "paul"];
+
+let server = http.createServer(function (request, response){
+    if(request.url === "/"){
+        response.end('<h2>Welcome!</h2>');
+    }else if(request.url === '/users'){
+        response.end("<h1>"+users + "</h1>");
+    }else{
+        response.end('<h1>Page Not Available</h1>');
+    }
+});
+
+server.listen(3000);
