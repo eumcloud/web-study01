8c4f9f2e89fdee0eeb8f79596d9a231f9e7f8ccc
- <livelch012@naver.com>
2021-11-05 22:00:46 +0900

    라우팅할때의 현실적인 부분.. 오브젝트에 있는 내용을 split으로 분배해준다?는 불편한 체험(을 통해 학습능률올리기..)

--- lesson/kiprojecti/assets/js/route.js
+++ lesson/kiprojecti/assets/js/route.js
@@ -10,6 +10,11 @@ let server = http.createServer(function (request, response){
         response.end('<h2>Welcome!</h2>');
     }else if(request.url === '/users'){
         response.end("<h1>"+users + "</h1>");
+    }else if(request.url.split('/')[1] === 'users'){//url : /users/1, /users/2, .. --> split 활용
+        let userIdx = request.url.split('/')[2];
+        let userName = users[userIdx -1];
+        
+        response.end('<h1>' + userName + '</h1>');
     }else{
         response.end('<h1>Page Not Available</h1>');
     }
