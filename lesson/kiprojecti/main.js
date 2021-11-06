7dfa904b7f8014db9828517ac41828033b82d516
- <livelch012@naver.com>
2021-11-06 20:52:17 +0900

    Nodejs 메인 라우터 샘플

--- lesson/kiprojecti/main.js
+++ lesson/kiprojecti/main.js
@@ -1,27 +1,28 @@
-const http = require("http"); //통신규약. 프로토콜.
+var express = require('express');
+var app = express();
+const users = ['Tom', 'Andy', 'Jessica', 'Paul'];
 
-//서버객체 생성.
-let server = http.createServer(function (request, response){
-  response.end("<h2>Hello Node.js!</h2>");
-}); 
-server.listen(3000); //포트번호 3000을 통해서 클라이언트 요청대기. 
-//ps. 서버안에서 동작하는 프로그램마다 사용하는 포트번호가 다르다.
-  //접속 테스트시 로컬 127.0.0.1:3000을 통해서 접속.
+app.get('/', (req, res)=>{
+   res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/users', (req, res)=>{res.end(`<h1>${users}</h1>`);});
+app.get('/second.html', (req, res)=>{res.sendFile(__dirname + '/pages/second.html')});
+app.get('/users:id', (req, res)=>{
+    const userName = users[request.params.id -1];
+    res.end(`<h1>${userName}</h1>`);
+});
+app.get('*', (req, res)=>{res.write("<script>alert('잘못된 경로입니다.')</script>");});
+
+var server = app.listen(3000, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log("Example app listening at http://%s:%s", host, port);
+});
+console.log('Server Start');
 
 
 
 
 
-
-// 참고)
-// URL : Uniform Resource Locator
-// 웹 상의 특정 자원의 위치를 나타내는 문자열
-// *특정자원 : HTML, CSS, JavaScript, 이미지, 영상 등
-//==>> URL의 구조
-//http://example.com/business/mart/item?category=14&id=2965
-//http - 스킴(scheme) ->프로토콜 이름 (클라이언트와 서버간의 통신규약. http 또는 https)
-//example.com - 호스트(host) -> 특정서버를 나나타냄
-//business/mart/item - 경로(path) -> 원하는 자원의 위치를 나타냄. business/mart디렉토리 안에 item 존재. 
-//그러나, item파일이 꼭 없어도 됨. = 서버가 자신만의 방식으로 해석한것.
-
-// ?category=14&id=2965 - 쿼리(query) ->서버에 요청할 때 원하는 것을 상세하게 표현하기 위해 사용.
